69f786fe9e94a4b30a0bebc7eaf262a12a26817c
Your Name <you@example.com>
2023-02-03 07:20:46 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var fullName = "Mideva Marrion"
+    println(fullName)
+
+    var age = 20
+    println(age)
+
+    var phoneNumber = "0113906150"
+    println(phoneNumber)
+
+    var weightInKg = 63
+    println(weightInKg)
+
+    var kenyanCitizen: Boolean = true
+    println(kenyanCitizen)
+}
